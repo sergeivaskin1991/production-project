4eca562462ab4fa0222bd548e9a6c60aad86cef7
Sergei Vaskin <svaskin@alfabank.ru>
2023-06-25 19:02:41 +0300

    semantics

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'],
+                ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 200 }],
